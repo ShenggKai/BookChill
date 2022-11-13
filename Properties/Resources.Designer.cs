34be6bc31048edd179db5d76432ed791ddc4865d
Sheng Kai <maimemdada@gmail.com>
2022-11-13 15:19:19 +0700

    remove unnecessary image and rename some images

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -103,16 +103,6 @@ namespace BookChill.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap BookChill {
-            get {
-                object obj = ResourceManager.GetObject("BookChill", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap books {
             get {
                 object obj = ResourceManager.GetObject("books", resourceCulture);
