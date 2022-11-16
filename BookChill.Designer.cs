83dbef36462aff40f6c3099f577bd0c16b04e600
Sheng Kai <maimemdada@gmail.com>
2022-11-16 08:21:12 +0700

    add label news

--- BookChill.Designer.cs
+++ BookChill.Designer.cs
@@ -56,6 +56,8 @@
             this.pCLose = new System.Windows.Forms.PictureBox();
             this.pMinimize = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.txtNews = new System.Windows.Forms.Label();
             this.panelMenu.SuspendLayout();
             this.panelSubMenuSca.SuspendLayout();
             this.panelSubMenuColec.SuspendLayout();
@@ -66,6 +68,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pBook)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pCLose)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pMinimize)).BeginInit();
+            this.panelMain.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelMenu
@@ -475,12 +478,34 @@
             this.timer1.Interval = 8000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // panelMain
+            // 
+            this.panelMain.BackColor = System.Drawing.Color.Transparent;
+            this.panelMain.Controls.Add(this.txtNews);
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain.Location = new System.Drawing.Point(272, 515);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(1648, 565);
+            this.panelMain.TabIndex = 4;
+            // 
+            // txtNews
+            // 
+            this.txtNews.AutoSize = true;
+            this.txtNews.Font = new System.Drawing.Font("Segoe UI", 22.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtNews.Location = new System.Drawing.Point(0, 0);
+            this.txtNews.Name = "txtNews";
+            this.txtNews.Padding = new System.Windows.Forms.Padding(10);
+            this.txtNews.Size = new System.Drawing.Size(170, 72);
+            this.txtNews.TabIndex = 0;
+            this.txtNews.Text = "Tin mới";
+            // 
             // BookChill
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.Controls.Add(this.panelMain);
             this.Controls.Add(this.panelHeading);
             this.Controls.Add(this.panelMenu);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -500,6 +525,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.pBook)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pCLose)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pMinimize)).EndInit();
+            this.panelMain.ResumeLayout(false);
+            this.panelMain.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -532,5 +559,7 @@
         private Button btnBarCode;
         private Button btnQrCode;
         private System.Windows.Forms.Timer timer1;
+        private Panel panelMain;
+        private Label txtNews;
     }
 }
