2172616819a87d3df64cb7c8310f35e9824650e2
Sheng Kai <maimemdada@gmail.com>
2022-11-16 22:13:26 +0700

    add van hoc form

---
+++ Child Form/VanHoc.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace BookChill.Child_Form
+{
+    partial class VanHoc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VanHoc));
+            this.SuspendLayout();
+            // 
+            // VanHoc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1630, 1033);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "VanHoc";
+            this.Text = "Văn học";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
