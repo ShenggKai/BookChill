c04a7c8eaa2068ecae59e9e9d0043c5a8461d1c0
Sheng Kai <maimemdada@gmail.com>
2022-11-19 07:57:25 +0700

    add form ngoai ngu, add books

--- Child Form/NgoaiNgu.Designer.cs
+++ Child Form/NgoaiNgu.Designer.cs
@@ -28,14 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NgoaiNgu));
             this.panelHeader = new System.Windows.Forms.Panel();
             this.pCLose = new System.Windows.Forms.PictureBox();
             this.txtNameChildForm = new System.Windows.Forms.Label();
             this.pMinimize = new System.Windows.Forms.PictureBox();
             this.cbSort = new System.Windows.Forms.ComboBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.bookItem4 = new BookChill.User_Control.BookItem();
+            this.bookItem5 = new BookChill.User_Control.BookItem();
+            this.bookItem6 = new BookChill.User_Control.BookItem();
+            this.bookItem7 = new BookChill.User_Control.BookItem();
+            this.bookItem8 = new BookChill.User_Control.BookItem();
+            this.bookItem9 = new BookChill.User_Control.BookItem();
+            this.bookItem10 = new BookChill.User_Control.BookItem();
             this.panelHeader.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pCLose)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pMinimize)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelHeader
@@ -73,9 +83,9 @@
             this.txtNameChildForm.Font = new System.Drawing.Font("Segoe UI Semibold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.txtNameChildForm.Location = new System.Drawing.Point(65, 10);
             this.txtNameChildForm.Name = "txtNameChildForm";
-            this.txtNameChildForm.Size = new System.Drawing.Size(431, 54);
+            this.txtNameChildForm.Size = new System.Drawing.Size(216, 54);
             this.txtNameChildForm.TabIndex = 1;
-            this.txtNameChildForm.Text = "Tư duy - Kỹ năng sống";
+            this.txtNameChildForm.Text = "Ngoại ngữ";
             // 
             // pMinimize
             // 
@@ -116,11 +126,119 @@
             this.cbSort.TabIndex = 9;
             this.cbSort.Text = "Sắp xếp theo";
             // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.bookItem4);
+            this.flowLayoutPanel1.Controls.Add(this.bookItem5);
+            this.flowLayoutPanel1.Controls.Add(this.bookItem6);
+            this.flowLayoutPanel1.Controls.Add(this.bookItem7);
+            this.flowLayoutPanel1.Controls.Add(this.bookItem8);
+            this.flowLayoutPanel1.Controls.Add(this.bookItem9);
+            this.flowLayoutPanel1.Controls.Add(this.bookItem10);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 84);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(40, 20, 20, 20);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1648, 912);
+            this.flowLayoutPanel1.TabIndex = 15;
+            // 
+            // bookItem4
+            // 
+            this.bookItem4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(129)))), ((int)(((byte)(160)))), ((int)(((byte)(103)))));
+            this.bookItem4.BookImage = ((System.Drawing.Image)(resources.GetObject("bookItem4.BookImage")));
+            this.bookItem4.BookName = "    Giáo Trình Chuẩn HSK 1";
+            this.bookItem4.Location = new System.Drawing.Point(70, 50);
+            this.bookItem4.Margin = new System.Windows.Forms.Padding(30);
+            this.bookItem4.Name = "bookItem4";
+            this.bookItem4.Price = "158.000 đ";
+            this.bookItem4.Rating = "4.8";
+            this.bookItem4.Size = new System.Drawing.Size(249, 365);
+            this.bookItem4.TabIndex = 3;
+            // 
+            // bookItem5
+            // 
+            this.bookItem5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(129)))), ((int)(((byte)(160)))), ((int)(((byte)(103)))));
+            this.bookItem5.BookImage = ((System.Drawing.Image)(resources.GetObject("bookItem5.BookImage")));
+            this.bookItem5.BookName = "Tomato Toeic Speaking Flow";
+            this.bookItem5.Location = new System.Drawing.Point(379, 50);
+            this.bookItem5.Margin = new System.Windows.Forms.Padding(30);
+            this.bookItem5.Name = "bookItem5";
+            this.bookItem5.Price = "238.000 đ";
+            this.bookItem5.Rating = "4.8";
+            this.bookItem5.Size = new System.Drawing.Size(249, 365);
+            this.bookItem5.TabIndex = 4;
+            // 
+            // bookItem6
+            // 
+            this.bookItem6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(129)))), ((int)(((byte)(160)))), ((int)(((byte)(103)))));
+            this.bookItem6.BookImage = ((System.Drawing.Image)(resources.GetObject("bookItem6.BookImage")));
+            this.bookItem6.BookName = "Tự Học 2000 Từ Vựng Tiếng Anh Theo Chủ Đề";
+            this.bookItem6.Location = new System.Drawing.Point(688, 50);
+            this.bookItem6.Margin = new System.Windows.Forms.Padding(30);
+            this.bookItem6.Name = "bookItem6";
+            this.bookItem6.Price = "65.000 đ";
+            this.bookItem6.Rating = "4.5";
+            this.bookItem6.Size = new System.Drawing.Size(249, 365);
+            this.bookItem6.TabIndex = 5;
+            // 
+            // bookItem7
+            // 
+            this.bookItem7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(129)))), ((int)(((byte)(160)))), ((int)(((byte)(103)))));
+            this.bookItem7.BookImage = ((System.Drawing.Image)(resources.GetObject("bookItem7.BookImage")));
+            this.bookItem7.BookName = "Tự Học 2000 Từ Vựng Tiếng Anh Theo Chủ Đề";
+            this.bookItem7.Location = new System.Drawing.Point(997, 50);
+            this.bookItem7.Margin = new System.Windows.Forms.Padding(30);
+            this.bookItem7.Name = "bookItem7";
+            this.bookItem7.Price = "188.000 đ";
+            this.bookItem7.Rating = "4.5";
+            this.bookItem7.Size = new System.Drawing.Size(249, 365);
+            this.bookItem7.TabIndex = 6;
+            // 
+            // bookItem8
+            // 
+            this.bookItem8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(129)))), ((int)(((byte)(160)))), ((int)(((byte)(103)))));
+            this.bookItem8.BookImage = ((System.Drawing.Image)(resources.GetObject("bookItem8.BookImage")));
+            this.bookItem8.BookName = "Doraemon Movie Story: Nobita Và Hòn Đảo Diệu Kì - Cuộc Phiêu Lưu Của Loài Thú";
+            this.bookItem8.Location = new System.Drawing.Point(1306, 50);
+            this.bookItem8.Margin = new System.Windows.Forms.Padding(30);
+            this.bookItem8.Name = "bookItem8";
+            this.bookItem8.Price = "188.000 đ";
+            this.bookItem8.Rating = "4.5";
+            this.bookItem8.Size = new System.Drawing.Size(249, 365);
+            this.bookItem8.TabIndex = 7;
+            // 
+            // bookItem9
+            // 
+            this.bookItem9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(129)))), ((int)(((byte)(160)))), ((int)(((byte)(103)))));
+            this.bookItem9.BookImage = ((System.Drawing.Image)(resources.GetObject("bookItem9.BookImage")));
+            this.bookItem9.BookName = "Doraemon Movie Story: Nobita Và Hòn Đảo Diệu Kì - Cuộc Phiêu Lưu Của Loài Thú";
+            this.bookItem9.Location = new System.Drawing.Point(70, 475);
+            this.bookItem9.Margin = new System.Windows.Forms.Padding(30);
+            this.bookItem9.Name = "bookItem9";
+            this.bookItem9.Price = "188.000 đ";
+            this.bookItem9.Rating = "4.5";
+            this.bookItem9.Size = new System.Drawing.Size(249, 365);
+            this.bookItem9.TabIndex = 8;
+            // 
+            // bookItem10
+            // 
+            this.bookItem10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(129)))), ((int)(((byte)(160)))), ((int)(((byte)(103)))));
+            this.bookItem10.BookImage = ((System.Drawing.Image)(resources.GetObject("bookItem10.BookImage")));
+            this.bookItem10.BookName = "Doraemon Movie Story: Nobita Và Hòn Đảo Diệu Kì - Cuộc Phiêu Lưu Của Loài Thú";
+            this.bookItem10.Location = new System.Drawing.Point(379, 475);
+            this.bookItem10.Margin = new System.Windows.Forms.Padding(30);
+            this.bookItem10.Name = "bookItem10";
+            this.bookItem10.Price = "188.000 đ";
+            this.bookItem10.Rating = "4.5";
+            this.bookItem10.Size = new System.Drawing.Size(249, 365);
+            this.bookItem10.TabIndex = 9;
+            // 
             // NgoaiNgu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1648, 996);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.panelHeader);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "NgoaiNgu";
@@ -131,6 +249,7 @@
             this.panelHeader.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pCLose)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pMinimize)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -142,5 +261,13 @@
         private Label txtNameChildForm;
         private PictureBox pMinimize;
         private ComboBox cbSort;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private User_Control.BookItem bookItem4;
+        private User_Control.BookItem bookItem5;
+        private User_Control.BookItem bookItem6;
+        private User_Control.BookItem bookItem7;
+        private User_Control.BookItem bookItem8;
+        private User_Control.BookItem bookItem9;
+        private User_Control.BookItem bookItem10;
     }
 }
