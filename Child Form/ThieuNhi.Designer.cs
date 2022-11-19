598a0960bb6facc1600ecd81afcfe929c6e4956d
Sheng Kai <maimemdada@gmail.com>
2022-11-19 08:19:10 +0700

    add code

--- Child Form/ThieuNhi.Designer.cs
+++ Child Form/ThieuNhi.Designer.cs
@@ -28,20 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.pCLose = new System.Windows.Forms.PictureBox();
+            this.txtNameChildForm = new System.Windows.Forms.Label();
+            this.pMinimize = new System.Windows.Forms.PictureBox();
+            this.cbSort = new System.Windows.Forms.ComboBox();
+            this.panelHeader.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pCLose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pMinimize)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.BackColor = System.Drawing.Color.Transparent;
+            this.panelHeader.Controls.Add(this.pCLose);
+            this.panelHeader.Controls.Add(this.txtNameChildForm);
+            this.panelHeader.Controls.Add(this.pMinimize);
+            this.panelHeader.Controls.Add(this.cbSort);
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHeader.Location = new System.Drawing.Point(0, 0);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(1648, 84);
+            this.panelHeader.TabIndex = 15;
+            // 
+            // pCLose
+            // 
+            this.pCLose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pCLose.BackColor = System.Drawing.Color.Transparent;
+            this.pCLose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pCLose.Image = global::BookChill.Properties.Resources.close;
+            this.pCLose.Location = new System.Drawing.Point(1601, 0);
+            this.pCLose.Name = "pCLose";
+            this.pCLose.Size = new System.Drawing.Size(40, 40);
+            this.pCLose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pCLose.TabIndex = 11;
+            this.pCLose.TabStop = false;
+            // 
+            // txtNameChildForm
+            // 
+            this.txtNameChildForm.AutoSize = true;
+            this.txtNameChildForm.Font = new System.Drawing.Font("Segoe UI Semibold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtNameChildForm.Location = new System.Drawing.Point(65, 10);
+            this.txtNameChildForm.Name = "txtNameChildForm";
+            this.txtNameChildForm.Size = new System.Drawing.Size(277, 54);
+            this.txtNameChildForm.TabIndex = 1;
+            this.txtNameChildForm.Text = "Sách thiếu nhi";
+            // 
+            // pMinimize
+            // 
+            this.pMinimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pMinimize.BackColor = System.Drawing.Color.Transparent;
+            this.pMinimize.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pMinimize.Image = global::BookChill.Properties.Resources.minimize;
+            this.pMinimize.Location = new System.Drawing.Point(1535, 0);
+            this.pMinimize.Name = "pMinimize";
+            this.pMinimize.Size = new System.Drawing.Size(40, 40);
+            this.pMinimize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pMinimize.TabIndex = 10;
+            this.pMinimize.TabStop = false;
+            // 
+            // cbSort
+            // 
+            this.cbSort.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(129)))), ((int)(((byte)(160)))), ((int)(((byte)(103)))));
+            this.cbSort.DropDownWidth = 3;
+            this.cbSort.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cbSort.ForeColor = System.Drawing.Color.White;
+            this.cbSort.FormattingEnabled = true;
+            this.cbSort.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.cbSort.ItemHeight = 41;
+            this.cbSort.Items.AddRange(new object[] {
+            "Giá từ cao đến thấp",
+            "Giá từ thấp đến cao",
+            "Sách được yêu thích",
+            "Sách mới xuất bản"});
+            this.cbSort.Location = new System.Drawing.Point(590, 17);
+            this.cbSort.MaxDropDownItems = 5;
+            this.cbSort.Name = "cbSort";
+            this.cbSort.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.cbSort.Size = new System.Drawing.Size(304, 49);
+            this.cbSort.Sorted = true;
+            this.cbSort.TabIndex = 9;
+            this.cbSort.Text = "Sắp xếp theo";
             // 
             // ThieuNhi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1630, 949);
+            this.ClientSize = new System.Drawing.Size(1648, 949);
+            this.Controls.Add(this.panelHeader);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "ThieuNhi";
             this.Text = "Thiếu nhi";
+            this.panelHeader.ResumeLayout(false);
+            this.panelHeader.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pCLose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pMinimize)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Panel panelHeader;
+        private PictureBox pCLose;
+        private Label txtNameChildForm;
+        private PictureBox pMinimize;
+        private ComboBox cbSort;
     }
 }
